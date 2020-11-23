b0ced272351989c53f28a3d4370c8c2128c8e21a
KoukiIijima <lle0nonnndebo@gmail.com>
2020-11-23 15:28:16 +0900

    [update]Display post list

---
+++ blog/src/actions/index.js
@@ -0,0 +1,19 @@
+import jsonPlaceholder from "../api/jsonPlaceholder";
+
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceholder.get("/posts");
+
+  dispatch({ type: "FETCH_POSTS", payload: response });
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({type: 'FETCH_USER', payload: response.data})
+};
+
+export const selectPost = () => {
+  return {
+    type: "SELECT_POST",
+  };
+};
--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,14 +1,31 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchPosts } from "../actions/fetchPosts";
+import { fetchPosts } from "../actions";
 
 class PostList extends React.Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
+
+  renderList() {
+    return this.props.posts.map(post => {
+      return (
+        <div key={post.id}>
+          <h2>description: {post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return <div>Post List</div>;
+    console.log(this.props.posts);
+    return <div>{this.renderList()}</div>;
   }
 }
 
-export default connect(null, { fetchPosts })(PostList);
+const mapStateToProp = (state) => {
+  return { posts: state.posts.data };
+};
+
+export default connect(mapStateToProp, { fetchPosts })(PostList);
